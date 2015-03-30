f3002d45cae317964fb13ba9acc9f027e0f0f558
Paul Jemmott <pjemmo1@students.towson.edu>
2015-03-29 23:54:04 -0400

    created the user and feed tables and the database connection  between them

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/add_user'
+
   root 'home#index'
 
   resources :posts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319064554) do
+ActiveRecord::Schema.define(version: 20150326155644) do
+
+  create_table "feeds", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,7 +27,13 @@ ActiveRecord::Schema.define(version: 20150319064554) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_feeds", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "feed_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -33,7 +46,6 @@ ActiveRecord::Schema.define(version: 20150319064554) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
